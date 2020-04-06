cc3dc370be177469136e99e0ee6e889fbacd7d3d
IreneGalca <54726113+IreneGalca@users.noreply.github.com>
2020-04-05 23:54:42 -0600

    NFAState added

--- 361_p2/fa/nfa/NFAState.java
+++ 361_p2/fa/nfa/NFAState.java
@@ -1,5 +1,66 @@
+/***************************************
+ * CS 361: P2
+ * @Authors Emma Lytle, Irene Galca
+ ***************************************/
+
 package fa.nfa;
 
-public class NFAState {
+import java.util.HashMap;
 
+import fa.nfa.NFAState;
+
+public class NFAState {
+	private HashMap<Character,NFAState> delta;//delta
+	private boolean isFinal;//remembers its type
+	private String name;
+
+	/******************************
+	 * Default constructor
+	 * @param name the state name
+	 ******************************/
+	public NFAState(String name, boolean isFinal){
+		initDefault(name);
+		isFinal = false;
+	}
+
+	private void initDefault(String name) {
+		// TODO Auto-generated method stub
+		this.name = name;
+		delta = new HashMap<Character, NFAState>();
+	}
+	
+	/****************************************************
+	 * Accessor for the state type
+	 * @return true if final and false otherwise
+	 ****************************************************/
+	public boolean isFinal(){
+		return isFinal;
+	}
+	
+
+	/*****************************************************
+	 * Add the transition from <code> this </code> object
+	 * @param onSymb the alphabet symbol
+	 * @param toState to DFA state
+	 *****************************************************/
+	public void addTrans(char onSymb, NFAState toState) {
+		delta.put(onSymb,  toState);
+	}
+	
+	/*************************************************************
+	 * Retrieves the state that <code>this</code> transitions to
+	 * on the given symbol
+	 * @param symb - the alphabet symbol
+	 * @return the new state 
+	 *************************************************************/
+	public NFAState getTo(char symb) {
+		NFAState ret = delta.get(symb);
+		if(ret == null) {
+			 System.err.println("ERROR: NFAState.getTo(char symb) returns null on " + symb + " from " + name);
+			 System.exit(2);
+		}
+		return delta.get(symb);
+	}
 }
+
+
