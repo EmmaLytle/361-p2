8f0316faa0ed9c244e2117f8a64aa0672a11343c
shinjiksi <50182135+shinjiksi@users.noreply.github.com>
2020-04-15 21:18:56 -0600

    Update NFAState.java

--- 361_p2/fa/nfa/NFAState.java
+++ 361_p2/fa/nfa/NFAState.java
@@ -7,81 +7,80 @@ import fa.State;
 import fa.nfa.NFAState;
 
 /***************************************
- * CS 361: P2
- * Defines what a NFAState is as well as extends State.
+ * CS 361: P2 Defines what a NFAState is as well as extends State.
+ * 
  * @Authors Emma Lytle, Irene Galca, Shinji Kasai
  ***************************************/
 public class NFAState extends State {
-	private HashMap<Character,HashSet<NFAState>> delta;
+	private HashMap<Character, HashSet<NFAState>> delta;
 
-	private boolean isFinal;//remembers its type
+	private boolean isFinal;// remembers its type
 
 	/******************************
 	 * Default constructor
+	 * 
 	 * @param name the state name
 	 ******************************/
-	
-	public NFAState(String name){
+
+	public NFAState(String name) {
 		initDefault(name);
 		isFinal = false;
 	}
 
-	 public NFAState(String name, boolean isFinal){
+	public NFAState(String name, boolean isFinal) {
 		initDefault(name);
 		this.isFinal = isFinal;
 	}
 
 	private void initDefault(String name) {
 		this.name = name;
-		delta = new HashMap<Character,HashSet<NFAState>> ();
+		delta = new HashMap<Character, HashSet<NFAState>>();
 	}
-	
+
 	/****************************************************
 	 * Accessor for the state type
+	 * 
 	 * @return true if final and false otherwise
 	 ****************************************************/
-	public boolean isFinal(){
+	public boolean isFinal() {
 		return isFinal;
 	}
 
 	/*****************************************************
 	 * Add the transition from <code> this </code> object
-	 * @param onSymb the alphabet symbol
+	 * 
+	 * @param onSymb  the alphabet symbol
 	 * @param toState to DFA state
 	 *****************************************************/
 	public void addTrans(char onSymb, NFAState toState) {
-		HashSet<NFAState>temp = delta.get(onSymb);
-		if(temp == null) {
+		HashSet<NFAState> temp = delta.get(onSymb);
+		if (temp == null) {
 			temp = new HashSet<NFAState>();
 		}
 		temp.add(toState);
-		
-		delta.put(onSymb,  temp);
+
+		delta.put(onSymb, temp);
 	}
 
-	public HashMap<Character,HashSet<NFAState>> getTrans()
-	{
+	public HashMap<Character, HashSet<NFAState>> getTrans() {
 		return delta;
 	}
-	
+
 	/*************************************************************
-	 * Retrieves the state that <code>this</code> transitions to
-	 * on the given symbol
+	 * Retrieves the state that <code>this</code> transitions to on the given symbol
+	 * 
 	 * @param symb - the alphabet symbol
-	 * @return the new state 
+	 * @return the new state
 	 *************************************************************/
 	public Set<NFAState> getTo(char symb) {
 
 		HashSet<NFAState> ret = delta.get(symb);
-		if(ret == null) {
-			 System.err.println("ERROR: NFAState.getTo(char symb) returns null on " + symb + " from " + name);
-			 System.exit(2);
+		if (ret == null) {
+			System.err.println("ERROR: NFAState.getTo(char symb) returns null on " + symb + " from " + name);
+			System.exit(2);
 		}
 		return delta.get(symb);
 
 	}
 
-	
 }
-
-
